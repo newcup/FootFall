48dd82d06f3d5bdb480a0567e8405b960ebcb664
JasonXZLiu <jason.XZ.liu@gmail.com>
2017-08-25 00:44:13 -0400

    try to add animation

--- docs/js/what.js
+++ docs/js/what.js
@@ -1,4 +1,53 @@
-$(window).scroll(function (event) {
-    var scroll = $(window).scrollTop(100);
-    alert("hi");
+var layer1 = $("#layer1");
+var layer2 = $("#layer2");
+var layer3 = $("#layer3");
+var layer4 = $("#layer4");
+var layer5 = $("#layer5");
+var layer6 = $("#layer6");
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    if (scroll > 600) {
+        layer1.animate({
+            '-webkit-transform' : 'translate(0px, -100px)',
+            '-moz-transform'    : 'translate(0px, -100px)',
+            '-ms-transform'     : 'translate(0px, -100px)',
+            '-o-transform'      : 'translate(0px, -100px)',
+            'transform'         : 'translate(0px, -100px)',
+        });
+        layer2.animate({
+            '-webkit-transform' : 'translate(0px, -50px)',
+            '-moz-transform'    : 'translate(0px, -50px)',
+            '-ms-transform'     : 'translate(0px, -50px)',
+            '-o-transform'      : 'translate(0px, -50px)',
+            'transform'         : 'translate(0px, -50px)',
+        });
+        layer3.animate({
+            '-webkit-transform' : 'translate(0px, 0px)',
+            '-moz-transform'    : 'translate(0px, 0px)',
+            '-ms-transform'     : 'translate(0px, 0px)',
+            '-o-transform'      : 'translate(0px, 0px)',
+            'transform'         : 'translate(0px, 0px)',
+        });
+        layer4.animate({
+            '-webkit-transform' : 'translate(0px, 50px)',
+            '-moz-transform'    : 'translate(0px, 50px)',
+            '-ms-transform'     : 'translate(0px, 50px)',
+            '-o-transform'      : 'translate(0px, 50px)',
+            'transform'         : 'translate(0px, 50px)',
+        });
+        layer5.animate({
+            '-webkit-transform' : 'translate(0px, 100px)',
+            '-moz-transform'    : 'translate(0px, 100px)',
+            '-ms-transform'     : 'translate(0px, 100px)',
+            '-o-transform'      : 'translate(0px, 100px)',
+            'transform'         : 'translate(0px, 100px)',
+        });
+        layer5.animate({
+            '-webkit-transform' : 'translate(0px, 150px)',
+            '-moz-transform'    : 'translate(0px, 150px)',
+            '-ms-transform'     : 'translate(0px, 150px)',
+            '-o-transform'      : 'translate(0px, 150px)',
+            'transform'         : 'translate(0px, 150px)',
+        });
+    }
 });
